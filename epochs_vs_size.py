8aa61f01addb9a9258cab9a299530dcc68ecd72b
Yosef Jaber <yusefj816@gmail.com>
2025-03-24 13:17:43 -0400

    added learning rate vs size script

--- epochs_vs_size.py
+++ epochs_vs_size.py
@@ -80,4 +80,82 @@ plt.ylabel("Epochs")
 plt.xticks(np.arange(3), ["Small", "Medium", "Large"])
 plt.yticks(np.arange(3), ["500", "250", "100"])
 # plt.grid(visible=True, color='white', linestyle='-', linewidth=0.5)
+# plt.show()
+
+
+
+
+
+#Epochs vs Size
+_100_small = 0
+_100_medium = 0
+_100_large = 0
+
+_250_small = 0
+_250_medium = 0
+_250_large = 0
+
+_500_small = 0
+_500_medium = 0
+_500_large = 0
+
+count = 0
+for i in range(len(results)):
+    model_name = results.iloc[i]["Model"]
+    mse_value = results.iloc[i]["MSE"]
+
+    if re.search("100", model_name) and re.search("small", model_name):
+        _100_small += mse_value
+        print(model_name)
+        count += 1
+    elif re.search("100", model_name) and re.search("medium", model_name):
+        _100_medium += mse_value
+    elif re.search("100", model_name) and re.search("large", model_name):
+        _100_large += mse_value
+
+    elif re.search("250", model_name) and re.search("small", model_name):
+        _250_small += mse_value
+    elif re.search("250", model_name) and re.search("medium", model_name):
+        _250_medium += mse_value
+    elif re.search("250", model_name) and re.search("large", model_name):
+        _250_large += mse_value
+
+    elif re.search("500", model_name) and re.search("small", model_name):
+        _500_small += mse_value
+    elif re.search("500", model_name) and re.search("medium", model_name):
+        _500_medium += mse_value
+    elif re.search("500", model_name) and re.search("large", model_name):
+        _500_large += mse_value
+    else:
+        print("ERROR")
+        
+_100_small /= count
+_100_medium /= count
+_100_large /= count
+
+_250_small /= count
+_250_medium /= count
+_250_large /= count
+
+_500_small /= count
+_500_medium /= count
+_500_large /= count
+
+top = np.array([_500_small, _500_medium, _500_large])
+middle = np.array([_250_small, _250_medium, _250_large])
+bottom = np.array([_100_small, _100_medium, _100_large])
+
+values = [_100_small, _250_small, _500_large, _250_small, _250_medium, _250_large, _100_small, _100_medium, _100_large]
+categories = ["500 small", "500"]
+
+grid = np.array([top, middle, bottom])
+
+plt.imshow(grid, cmap='hot', interpolation='nearest')
+plt.colorbar()
+plt.title("Relationship of Size and Epochs")
+plt.xlabel("Size")
+plt.ylabel("Epochs")
+plt.xticks(np.arange(3), ["Small", "Medium", "Large"])
+plt.yticks(np.arange(3), ["500", "250", "100"])
+# plt.grid(visible=True, color='white', linestyle='-', linewidth=0.5)
 plt.show()
---
+++ learning_rate_vs_size.py
@@ -0,0 +1,102 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import re
+
+results = pd.read_csv("results.csv")
+Models = results["Model"]
+MSE = results["MSE"]
+R2 =  results["R^2"]
+MAE =  results["MAE"]
+CV =  results["CV"]
+Count =  results["Count"]
+
+
+#Learning Rate vs Size
+small_0_001 = 0
+small_0_0005 = 0
+small_0_0001 = 0
+small_0_00005 = 0
+
+medium_0_001 = 0
+medium_0_0005 = 0
+medium_0_0001 = 0
+medium_0_00005 = 0
+
+large_0_001 = 0
+large_0_0005 = 0
+large_0_0001 = 0
+large_0_00005 = 0
+
+count = 0
+for i in range(len(results)):
+    model_name = results.iloc[i]["Model"]
+    mse_value = results.iloc[i]["MSE"]
+
+    if re.search("small", model_name) and re.search("0.001", model_name):
+        small_0_001 += mse_value
+        count += 1
+    elif re.search("small", model_name) and re.search("0.0005", model_name):
+        small_0_0005 += mse_value
+    elif re.search("small", model_name) and re.search("0.0001", model_name):
+        small_0_0001 += mse_value
+    elif re.search("small", model_name) and re.search("5e-05", model_name):
+        small_0_00005 += mse_value
+
+    elif re.search("medium", model_name) and re.search("0.001", model_name):
+        medium_0_001 += mse_value
+    elif re.search("medium", model_name) and re.search("0.0005", model_name):
+        medium_0_0005 += mse_value
+    elif re.search("medium", model_name) and re.search("0.0001", model_name):
+        medium_0_0001 += mse_value
+    elif re.search("medium", model_name) and re.search("5e-05", model_name):
+        medium_0_00005 += mse_value
+
+    elif re.search("large", model_name) and re.search("0.001", model_name):
+        large_0_001 += mse_value
+    elif re.search("large", model_name) and re.search("0.0005", model_name):
+        large_0_0005 += mse_value
+    elif re.search("large", model_name) and re.search("0.0001", model_name):
+        large_0_0001 += mse_value
+    elif re.search("large", model_name) and re.search("5e-05", model_name):
+        large_0_00005 += mse_value
+    else:
+        print("ERROR")
+        
+small_0_001 /= count
+small_0_0005 /= count
+small_0_0001 /= count
+small_0_00005 /= count
+
+medium_0_001 /= count
+medium_0_0005 /= count
+medium_0_0001 /= count
+medium_0_00005 /= count
+
+large_0_001 /= count
+large_0_0005 /= count
+large_0_0001 /= count
+large_0_00005 /= count
+
+
+# x = [_500_0_001, _500_0_0005, _500_0_0001, _500_0_00005, _250_0_001, _250_0_0005, _250_0_0001, _250_0_00005, _100_0_001, _100_0_0005, _100_0_0001, _100_0_00005]
+# for i in range(len(x)):
+#     print(x[i])
+
+
+
+top = np.array([large_0_001, large_0_0005, large_0_0001, large_0_00005])
+middle = np.array([medium_0_001, medium_0_0005, medium_0_0001, medium_0_00005])
+bottom = np.array([small_0_001, small_0_0005, small_0_0001, small_0_00005])
+
+grid = np.array([top, middle, bottom])
+
+plt.imshow(grid, cmap='hot', interpolation='nearest')
+plt.colorbar()
+plt.title("Learning Rate vs Size Heatmap against MSE")
+plt.xlabel("Learning Rate")
+plt.ylabel("Size")
+plt.xticks(np.arange(4), ["0.001", "0.0005", "0.0001", "0.00005"])
+plt.yticks(np.arange(3), ["Large", "Medium", "Small"])
+# plt.grid(visible=True, color='white', linestyle='-', linewidth=0.5)
+plt.show()
